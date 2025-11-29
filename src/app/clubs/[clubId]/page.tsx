06e36df7012231f234a6cfc7ebff4342ac63e23e
joshuasilva414 <joshuasilva414@gmail.com>
2025-11-29 10:06:01 -0600

    show proof of sign-in

--- src/app/clubs/[clubId]/page.tsx
+++ src/app/clubs/[clubId]/page.tsx
@@ -1,4 +1,18 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
 export default async function Page({ params }: { params: { clubId: string } }) {
   const { clubId } = await params;
-  return <div>Club {clubId}</div>;
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session) {
+    return <div>Unauthorized</div>;
+  }
+
+  return <div>
+            <h2>Club {clubId}</h2>
+            <p>Session: {session.user?.email}</p>
+  </div>;
 }
