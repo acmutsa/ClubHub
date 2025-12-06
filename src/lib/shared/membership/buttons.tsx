145e9008863574412f5afa67ca4aaf51242b5928
joshuasilva414 <joshuasilva414@gmail.com>
2025-12-05 20:16:25 -0600

    add proper loading state

--- src/lib/shared/membership/buttons.tsx
+++ src/lib/shared/membership/buttons.tsx
@@ -2,10 +2,11 @@
 
 import { joinClub, leaveClub } from "@/actions/membership";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import { useAction } from "next-safe-action/hooks";
 
 export function JoinClubButton({ clubId }: { clubId: number }) {
-  const { execute } = useAction(joinClub.bind(null, clubId));
+  const { execute, isPending } = useAction(joinClub.bind(null, clubId));
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -14,13 +15,15 @@ export function JoinClubButton({ clubId }: { clubId: number }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Button variant="default">Join Club</Button>
+      <Button variant="default" disabled={isPending} className="w-full">
+        {isPending ? <Spinner /> : "Join Club"}
+      </Button>
     </form>
   );
 }
 
 export function LeaveClubButton({ clubId }: { clubId: number }) {
-  const { execute } = useAction(leaveClub.bind(null, clubId));
+  const { execute, isPending } = useAction(leaveClub.bind(null, clubId));
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,7 +32,9 @@ export function LeaveClubButton({ clubId }: { clubId: number }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Button variant="destructive">Leave Club</Button>
+      <Button variant="destructive" disabled={isPending} className="w-full">
+        {isPending ? <Spinner /> : "Leave Club"}
+      </Button>
     </form>
   );
 }
