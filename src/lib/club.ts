034461713d0701604b1baa016f9ffb7048bc79ba
joshuasilva414 <joshuasilva414@gmail.com>
2025-12-14 18:42:41 -0600

    add empty clubId check

--- src/lib/club.ts
+++ src/lib/club.ts
@@ -3,6 +3,9 @@ import { clubs } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export const getClub = async (clubId: string) => {
+  if (!clubId?.trim()) {
+    return null;
+  }
   const club = await db.query.clubs.findFirst({ where: eq(clubs.id, clubId) });
-  return club;
+  return club || null;
 };
