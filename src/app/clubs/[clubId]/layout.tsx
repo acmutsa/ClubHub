2e0bde3694988fec5168b88bb7cf14f073b03cdf
deeg454 <diego.medina@my.utsa.edu>
2025-12-16 23:27:11 -0600

    final navbar and footer design

--- src/app/clubs/[clubId]/layout.tsx
+++ src/app/clubs/[clubId]/layout.tsx
@@ -1,25 +1,43 @@
 import Navbar from "@/components/navbar";
-import { db } from "@/db";
-import { clubs } from "@/db/schema";
-import { eq } from "drizzle-orm";
-
+import Footer from "@/components/footer";
+import { getClub } from "@/lib/club";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import isClubAdmin from "@/lib/membership";
 export default async function Layout({
     children,
     params,
 }: Readonly<{
     children: React.ReactNode;
-    params: {
-        clubId: string;
-    };
+    params: Promise<{ clubId: string }>;
 }>) {
-
-    const club = await db.query.clubs.findFirst({
-        where: eq(clubs.id, params.clubId),
+    const { clubId } = await params;
+    const session = await auth.api.getSession({
+        headers: await headers(),
     });
+    if (!session) {
+        return redirect("/sign-in");
+    }
+    const user = session.user;
+
+    const userRole = await isClubAdmin(user.id, clubId);
+    if (!userRole) {
+        return redirect("/clubs");
+    }
+
+
+    const club = await getClub(clubId);
+    if (!club) {
+        return redirect("/clubs");
+    }
     return (
-        <div>
-            <Navbar clubName={club?.name ?? ""} clubId={club?.id} />
-            {children}
+        <div className="flex flex-col min-h-screen">
+            <Navbar clubName={club.name} clubId={clubId} userType={userRole ? "admin" : "member"} />
+            <main className="flex-1">
+                {children}
+            </main>
+            <Footer clubId={clubId} clubName={club.name} />
         </div>
     );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,33 +1,83 @@
 import Link from "next/link";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
-import SignOutButton from "./sign-out-button";
 import { redirect } from "next/navigation";
+import { Button } from "./ui/button";
+import { UserIcon } from "lucide-react";
+import SignOutButton from "./sign-out-button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { DropdownSwitcher } from "./ThemeSwitcher";
 interface NavbarProps {
   clubName: string;
-  clubId: number;
+  clubId: string;
+  userType: string;
 }
 
-export default async function Navbar({ clubName, clubId }: NavbarProps) {
+export default async function Navbar({
+  clubName,
+  clubId,
+  userType,
+}: NavbarProps) {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+
   if (!session) {
-    return redirect("/sign-in");
+    redirect("/sign-in");
   }
-  const userType = "admin";
+
   return (
-    <div className="min-h-16 bg-white">
-      <div className="flex items-center justify-between p-2 border-b rounded-lg">
-        <Link href="/">{clubName}</Link>
-        <div className="flex items-center gap-2"></div>
-        <div className="flex items-center gap-2">
-          <Link href="/clubs">Clubs</Link>
-          <Link href="/profile">Profile</Link>
-          {userType === "admin" && <Link href={`/clubs/${clubId}/admin`}>Admin</Link>}
-          <SignOutButton />
+    <header className="h-16 bg-background border-b border-border">
+      <div className="h-full flex items-center justify-between px-4">
+        <Link href={`/clubs/${clubId}`} className=" font-bold text-2xl">
+          {clubName}
+        </Link>
+
+        <div className="h-full flex items-center gap-4">
+          {userType === "admin" && (
+            <Button variant="link" asChild>
+              <Link className="font-semibold" href={`/clubs/${clubId}/admin`}>Admin</Link>
+            </Button>
+          )}
+
+          <Button variant="link" asChild>
+            <Link className="font-semibold" href={`/clubs/${clubId}/events`}>Events</Link>
+          </Button>
+
+          <Button variant="link" asChild>
+            <Link className="font-semibold" href={`/clubs/${clubId}/members`}>Members</Link>
+          </Button>
+
+          <DropdownMenu modal={false} >
+            <DropdownMenuTrigger asChild>
+              <div className="h-full flex items-center cursor-pointer">
+                <div className="flex items-center justify-center h-6 w-6">
+                  <UserIcon className="h-4 w-4" />
+                </div>
+              </div>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent side="bottom" align="end">
+              <DropdownMenuItem asChild>
+                <Link href={`/clubs/${clubId}/profile`} className="w-full">
+                  Settings
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <DropdownSwitcher />
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <SignOutButton />
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
