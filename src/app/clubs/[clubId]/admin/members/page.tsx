e99e55c898492c0ac1a0a4c6799c78e884e565b6
deeg454 <diego.medina@my.utsa.edu>
2026-02-12 08:43:54 -0600

    Table Component Added

--- src/app/clubs/[clubId]/admin/members/page.tsx
+++ src/app/clubs/[clubId]/admin/members/page.tsx
@@ -1,3 +1,41 @@
-export default function Page() {
-  return <div>Members</div>;
+import MemberDataTable from "@/components/clubs/admin/members-table";
+import { getMembers } from "@/lib/queries/club";
+import isClubAdmin from "@/lib/membership";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { memberColumns } from "@/components/clubs/admin/user-columns";
+
+export default async function Page({
+  params,
+}: {
+  params: { clubId: string };
+}) {
+
+  const { clubId } = await params;
+
+  const sessionData = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!sessionData) {
+    redirect("/login");
+  }
+
+  const user = sessionData.user;
+
+  const isAdmin = await isClubAdmin(clubId, user.id);
+  //add authentication here
+
+  const members = await getMembers(clubId);
+
+
+  return (
+    <div className="flex flex-col p-4">
+      <MemberDataTable
+        columns={memberColumns}
+        data={members ?? []}
+      />
+    </div>
+  );
 }
--- src/lib/queries/club.ts
+++ src/lib/queries/club.ts
@@ -1,11 +1,25 @@
 import { db } from "@/db/index";
-import { clubs } from "@/db/schema";
+import { clubs,membership,user } from "@/db/schema";
 import { eq } from "drizzle-orm";
-
 export async function getClub(clubId: string) {
   if (!clubId?.trim()) {
     return null;
   }
   const club = await db.query.clubs.findFirst({ where: eq(clubs.id, clubId) });
   return club || null;
+}
+
+export async function getMembers(clubId: string){
+  if(!clubId?.trim()){
+    return null
+  }
+  const members = await db.select({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: membership.role,
+        club: membership.clubId
+      }).from(membership).innerJoin(user,eq (user.id,membership.userId)).
+      where(eq(membership.clubId,clubId));
+  return members;
 }
