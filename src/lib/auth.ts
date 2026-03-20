de5d88940b8d2e9b085547ba42e43f06ba8513cc
deeg454 <diego.medina@my.utsa.edu>
2026-03-20 00:46:23 -0500

    reset password using resend

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { db } from "@/db";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { Resend } from 'resend'; 
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
   user: {
@@ -13,14 +15,18 @@ export const auth = betterAuth({
       },
     },
   },
-  emailAndPassword: {
+    emailAndPassword: {
     enabled: true,
-    async sendResetPassword(data, request) {
-
-            // Send an email to the user with a link to reset their password
-
-        },
+    sendResetPassword: async ({ user, url }) => {
+      await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: "diego.medina@my.utsa.edu",
+        subject: "Reset password",
+        html: `<a href="${url}">Reset</a>`,
+      });
+    },
   },
+  
   database: drizzleAdapter(db, {
     provider: "sqlite", // or "mysql", "sqlite"
   }),
