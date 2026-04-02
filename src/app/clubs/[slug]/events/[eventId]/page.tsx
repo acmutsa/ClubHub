6fa58b5343561c560c795542d04049459666039b
Miguel-Oseguera <145619835+Miguel-Oseguera@users.noreply.github.com>
2026-04-02 14:00:20 -0500

    added org event page

---
+++ src/app/clubs/[slug]/events/[eventId]/page.tsx
@@ -0,0 +1,26 @@
+import { notFound } from "next/navigation";
+import { getMemberEvent } from "@/lib/queries/events";
+import { EventDetailView } from "@/components/clubs/event-detail-view";
+
+interface EventDetailsPageProps {
+  params: Promise<{ slug: string; eventId: string }>;
+}
+
+export default async function EventDetailsPage({
+  params,
+}: EventDetailsPageProps) {
+  const { slug, eventId } = await params;
+  const eventIdNumber = parseInt(eventId, 10);
+
+  if (isNaN(eventIdNumber)) {
+    notFound();
+  }
+
+  const event = await getMemberEvent(slug, eventIdNumber);
+
+  if (!event) {
+    notFound();
+  }
+
+  return <EventDetailView event={event} />;
+}
